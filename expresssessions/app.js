74e6a29198c16068aa4ac2c4f36f86ff004aa521
James Collerton <james.collerton@bbc.co.uk>
2021-02-12 13:56:14 +0000

    Added express sessions

--- expresssessions/app.js
+++ expresssessions/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,15 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(session({
+  secret: 'This is my secret',
+  resave: false,
+  saveUninitialized: true,
+  // This should really be true, 
+  // it is false for the demo.
+  cookie: { secure: false }
+}))
 
 app.use(logger('dev'));
 app.use(express.json());
